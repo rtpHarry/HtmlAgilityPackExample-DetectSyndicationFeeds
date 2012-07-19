d94beffbb3d7c28840a6ca85a1959ba189d58627
rtpHarry <rtpharry@hotmail.com>
2012-07-19 22:49:27 +0100

    Initial commit

---
+++ App_Code/SyndicationFeedsDataObject.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Summary description for SyndicationFeedsDataObject
+/// </summary>
+[DataObject(true)]
+public class SyndicationFeedsDataObject
+{
+    public string FeedUrl { get; set; }
+    public string Title { get; set; }
+    public string MimeType { get; set; }
+
+    [DataObjectMethod(DataObjectMethodType.Select, true)]
+    public static IEnumerable<SyndicationFeedsDataObject> Select(string WebsiteUrl)
+    {
+        // parse the url
+        WebsiteUrl = ParseUrl(WebsiteUrl);
+
+        if (!IsValidUrl(WebsiteUrl))
+        {
+            return null;
+        }
+
+        // retrieve the website html
+        HtmlDocument doc = RetrieveHtml(WebsiteUrl);
+
+        // extract all tags
+        HtmlNodeCollection feeds = SelectFeeds(doc);
+
+        if (!IsValidFeedCollection(feeds))
+        {
+            return null;
+        }
+
+        // convert extracted tags to SyndicationFeedsDataObjects
+        var query = ConvertNodesToSyndicationFeeds(feeds);
+
+        // return the data
+        return query;
+    }
+
+    private static string ParseUrl(string WebsiteUrl)
+    {
+        if (string.IsNullOrWhiteSpace(WebsiteUrl))
+        {
+            return string.Empty;
+        }
+
+        if (!Regex.IsMatch(WebsiteUrl, "^http://", RegexOptions.IgnoreCase))
+        {
+            WebsiteUrl = "http://" + WebsiteUrl;
+        }
+
+        try
+        {
+            Uri TestUrl = new Uri(WebsiteUrl);
+        }
+        catch (UriFormatException)
+        {
+            WebsiteUrl = string.Empty;
+        }
+
+        return WebsiteUrl;
+    }
+
+    private static bool IsValidUrl(string WebsiteUrl)
+    {
+        return !string.IsNullOrWhiteSpace(WebsiteUrl);
+    }
+
+    private static HtmlDocument RetrieveHtml(string WebsiteUrl)
+    {
+        HtmlWeb hw = new HtmlWeb();
+        return hw.Load(WebsiteUrl);
+    }
+
+    private static HtmlNodeCollection SelectFeeds(HtmlDocument doc)
+    {
+        return doc.DocumentNode.SelectNodes("//link[(@type='application/rss+xml' or @type='application/atom+xml') and @rel='alternate']");
+    }
+
+    private static bool IsValidFeedCollection(HtmlNodeCollection feeds)
+    {
+        return feeds != null;
+    }
+
+    private static IEnumerable<SyndicationFeedsDataObject> ConvertNodesToSyndicationFeeds(HtmlNodeCollection feeds)
+    {
+        var query = from link in feeds
+                    select new SyndicationFeedsDataObject
+                    {
+                        FeedUrl = link.Attributes["href"].Value,
+                        Title = link.Attributes["title"].Value,
+                        MimeType = link.Attributes["type"].Value
+                    };
+        return query;
+    }
+}
---
+++ Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Html Agility Pack Example - Extract All Syndication Feeds From A Web Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>
+            Html Agility Pack Example - Extract All Syndication Feeds From A Web Page</h1>
+        <p>
+            This example shows you how to use Html Agility Pack to load a remote url and parse
+            out the urls of all syndication feeds it contains.</p>
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="URL:" />
+            <asp:TextBox ID="Url" runat="server" Text="http://www.cnn.com/"></asp:TextBox>
+            <asp:Button ID="RetrieveButton" runat="server" Text="Get Feeds"/>
+        </div>
+        <asp:GridView ID="ResultsGrid" runat="server" DataSourceID="ObjectDataSource1">
+        </asp:GridView>
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" 
+            OldValuesParameterFormatString="original_{0}" SelectMethod="Select" 
+            TypeName="SyndicationFeedsDataObject">
+            <SelectParameters>
+                <asp:ControlParameter ControlID="Url" Name="WebsiteUrl" PropertyName="Text" 
+                    Type="String" />
+            </SelectParameters>
+        </asp:ObjectDataSource>
+    </div>
+    </form>
+</body>
+</html>
